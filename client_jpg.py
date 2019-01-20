16fceea2d722efd62b9f21996d8b4f9140a0e398
Aviv <avivlerner10@gmail.com>
2019-01-20 21:57:35 +0200

    Sending the image from the memory and not from file

--- client_jpg.py
+++ client_jpg.py
@@ -1,3 +1,4 @@
+import pickle
 import socket
 
 IP = '127.0.0.1'
@@ -12,20 +13,29 @@ d = 0
 
 # number of paclets
 CountC = my_socket.recv(4096)
-numOfChunks = 8192 # 4096
+numOfBytesInChunk = 8192 # 4096
 tillC = CountC.decode('utf8')
-tillCC = int(tillC)
+numOfChunks = int(tillC)
+numOfChunks = numOfChunks + 1
 print("Receiving packets will start now if file exists.")
 # print(
 #   "Timeout is 15 seconds so please wait for timeout at the end.")
-while tillCC != 0:
-    ClientBData = my_socket.recv(numOfChunks)
-    dataS = BigC.write(ClientBData)
+
+bytes = bytes()
+while numOfChunks != 0:
+    chunkData = my_socket.recv(numOfBytesInChunk)
+
+    bytes += chunkData
+    # dataS = BigC.write(chunkData)
     d += 1
-    print("Received packet number:" + str(d))
-    tillCC = tillCC - 1
-print(
-    "New Received file closed. Check contents in your directory.")
+    print("Received packet number: {} with size of: {}".format(str(d), len(chunkData)))
+    numOfChunks = numOfChunks - 1
+
+print("Received data, number of bytes: " + str(len(bytes)))
+image = pickle.loads(bytes)
+image.save(r'screen.jpg')
+# dataS = BigC.write(image)
+print("New Received file closed. Check contents in your directory.")
 BigC.close()
 
 # with open(file_name, 'wb') as file:
--- server_jpg.py
+++ server_jpg.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import pickle
 import socket
 import time
 
@@ -21,32 +22,36 @@ startTime = time.clock()
 print('Starting time: {}'.format(datetime.datetime.now().time()))
 
 im = PIL.ImageGrab.grab()
-im.save(r'screen.jpg')
-
-g = r'C:\PythonProjects\ScreenShot_POC\screen.jpg'
-
+# im.save(r'screen.jpg')
+# g = r'C:\PythonProjects\ScreenShot_POC\screen.jpg'
+imageAsBytes = pickle.dumps(im)
 c = 0
-sizeS = os.stat(g)
-sizeSS = sizeS.st_size  # number of packets
-print("File size in bytes:" + str(sizeSS))
-numOfChunks = 8192 # 4096
-NumS = int(sizeSS / numOfChunks)
-NumS = NumS + 1
-tillSS = str(NumS)
-tillSSS = tillSS.encode('utf8')
-client_socket.send(tillSSS)
+imageSize = len(imageAsBytes)  # number of packets
+print("Image size in bytes: " + str(imageSize))
+numOfBytesInChunk = 8192 # 4096
+numOfChunks = int(imageSize / numOfBytesInChunk)
+numOfChunks = numOfChunks + 1
+numOfChunksStr = str(numOfChunks)
+print("Number of chunks: " + numOfChunksStr)
+numOfChunksStrEncoded = numOfChunksStr.encode('utf8')
+client_socket.send(numOfChunksStrEncoded)
 
-check = int(NumS)
-GetRunS = open(g, "rb")
-while check != 0:
-    RunS = GetRunS.read(numOfChunks)
-    client_socket.send(RunS)
+check = int(numOfChunks)
+# GetRunS = open(g, "rb")
+start = 0
+end = numOfBytesInChunk
+while check != 1:
+    currentChunk = imageAsBytes[start:end]
+    client_socket.send(currentChunk)
+    start += numOfBytesInChunk
+    end += numOfBytesInChunk
     c += 1
     check -= 1
-    print("Packet number:" + str(c))
-    print("Data sending in process:")
-GetRunS.close()
-print("Sent from Server - Get function")
+    print("Sending packet number: {} with size of: {}".format(str(c), len(currentChunk)))
+c += 1
+currentChunk = imageAsBytes[start:]
+print("Sending packet number: {} with size of: {}".format(str(c), len(currentChunk)))
+client_socket.send(currentChunk)
 print('End time: {}'.format(datetime.datetime.now().time()))
 
 # path = r'C:\Users\AMIT\PycharmProjects\project\screen.jpg'
